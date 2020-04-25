fbf262bee40bad14038d6e873474efc52f0cfb52
Auzzzz <s3544232@student.rmit.edu.au>
2020-04-25 07:52:21 +0100

    A1

--- menu.py
+++ menu.py
@@ -16,7 +16,8 @@ class Menu:
             print()
 
             if(selection == "1"):
-                self.listPeople()
+                #self.listPeople()
+		print("Hello")
             elif(selection == "2"):
                 self.insertPerson()
             elif(selection == "3"):
@@ -32,6 +33,14 @@ class Menu:
             for person in db.getPeople():
                 print("{:<15} {}".format(person[0], person[1]))
 
+    def insertPerson(self):
+        print("--- Insert Person ---")
+        name = input("Enter the person's name: ")
+        with DatabaseUtils() as db:
+            if(db.insertPerson(name)):
+                print("{} inserted successfully.".format(name))
+            else:
+                print("{} failed to be inserted.".format(name))
 
 if __name__ == "__main__":
     Menu().main()
