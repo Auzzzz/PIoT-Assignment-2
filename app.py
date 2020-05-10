6151d81c5bfe30250db032ea50f8fcffd029aac3
Auzzzz <s3544232@student.rmit.edu.au>
2020-05-10 14:43:22 +0100

    login works

--- app.py
+++ app.py
@@ -9,7 +9,7 @@
 #if __name__ == "__main__":
 #	app.run(debug=True)
 
-
+#https://flask.palletsprojects.com/en/1.1.x/quickstart/
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -17,10 +17,10 @@ import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
+# Secrect key for sec
 app.secret_key = 'your secret key'
 
-# Enter your database connection details below
+### Database Conn ###
 app.config['MYSQL_HOST'] = '34.87.245.145'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'banana192'
@@ -29,6 +29,31 @@ app.config['MYSQL_DB'] = 'carshare'
 # Intialize MySQL
 mysql = MySQL(app)
 
+### Login ###
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html', msg='')
+    #error message
+    msg = ''
+    #Check for when user submits form
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        #Capture the form data
+        username = request.form['username']
+        password = request.form['password']
+        
+        #Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from users where username = %s and password = %s', (username, password,))
+        account = cursor.fetchone()
+        # If account exists in database
+        if account:
+            # Create session data to keep track of user 
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # move user to home page 
+            return 'Logged in successfully!' #currently testing
+        else:
+            #error message
+            msg = 'Incorrect username/password!'
+    #dispay the login form and any message
+    return render_template('index.html', msg=msg)
