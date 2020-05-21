3cf9677a1f588af949da4002b4b3c8cb845c289e
Auzzzz <s3544232@student.rmit.edu.au>
2020-05-21 10:35:38 +0100

    booking (minus api)

--- lib/flask_api.py
+++ lib/flask_api.py
@@ -7,7 +7,6 @@ from flask_httpauth import HTTPBasicAuth
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 from passlib.hash import sha256_crypt
 
-
 ##https://requests.readthedocs.io/en/master/user/authentication/
 
 api = Blueprint("api", __name__)
@@ -15,7 +14,7 @@ api = Blueprint("api", __name__)
 db = SQLAlchemy()
 ma = Marshmallow()
 auth = HTTPBasicAuth()
-
+### Users ###
 # Declaring the model.
 class Person(db.Model):
     __tablename__ = "users"
@@ -61,7 +60,7 @@ class PersonSchema(ma.Schema):
 personSchema = PersonSchema()
 personsSchema = PersonSchema(many = True)
 
-# Endpoint to show all people.
+# Endpoint to show all people. #ADMIN
 @api.route("/api/person", methods = ["GET"])
 def getPeople():
     people = Person.query.all()
@@ -69,7 +68,7 @@ def getPeople():
     print(result)
     return jsonify(result)
 
-# Endpoint to get person by id.
+# Endpoint to get person by id. #ADMIN
 @api.route("/api/person/<id>", methods = ["GET"])
 def getPerson(id):
     person = Person.query.get(id)
@@ -138,8 +137,154 @@ def get_auth_token():
 @api.route('/api/login')
 @auth.login_required
 def get_resource():
-    return jsonify({'userid':g.user.userid, 'username':g.user.username})
+    return jsonify({'userid':g.user.userid, 'username':g.user.username, 'email':g.user.email, 'name':g.user.name})
     
 
 
+### Cars ###
+#Car
+class Car(db.Model):
+    __tablename__ = "cars"
+    carid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    colour = db.Column(db.VARCHAR(100))
+    seats = db.Column(db.Integer)
+    location = db.Column(db.VARCHAR(100))
+    cph = db.Column(db.Integer)
+    car_make_makeid = db.Column(db.Integer, db.ForeignKey('car_make.makeid'))
+    car_type_typeid = db.Column(db.Integer, db.ForeignKey('car_type.typeid'))
 
+def __init__(self, colour, seats, location, cph, car_make_makeid, car_type_typeid ):
+        self.colour = colour
+        self.seats = seats
+        self.location = location
+        self.cph = cph
+        self.car_make_makeid = car_make_makeid
+        self.car_type_typeid = car_type_typeid
+
+class CarSchema(ma.Schema):    
+    class Meta:
+        # Fields to expose.
+        fields = ("carid", "colour", "location", "cph", "car_make_makeid", "car_type_typeid" )
+
+carSchema = CarSchema()
+carsSchema = CarSchema(many = True)
+#Car Make
+class CarMake(db.Model):
+    __tablename__ ="car_make"
+    makeid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    make = db.Column(db.VARCHAR(100))
+
+    def __init__(self, make):
+        self.make = make
+
+class CarMakeSchema(ma.Schema):    
+    class Meta:
+        # Fields to expose.
+        fields = ("makeid", "make")
+
+carmakeSchema = CarMakeSchema()
+carmakesSchema = CarMakeSchema(many = True)
+
+
+#Car Type
+class CarType(db.Model):
+    __tablename__ ="car_type"
+    typeid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    type = db.Column(db.VARCHAR(100))
+
+    def __init__(self, type):
+        self.type = type
+
+class CarTypeSchema(ma.Schema):    
+    class Meta:
+        # Fields to expose.
+        fields = ("typeid", "type")
+
+cartypeSchema = CarTypeSchema()
+cartypesSchema = CarTypeSchema(many = True)
+
+#Booking
+class Booking(db.Model):
+    __tablename__ = "booking"
+    bookingid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    userid = db.Column(db.Integer, db.ForeignKey('users.usersid'))
+    carid = db.Column(db.Integer, db.ForeignKey('cars.carid'))
+    bdate = db.Column(db.DATE)
+    stime = db.Column(db.TIME)
+    etime = db.Column(db.TIME)
+    bookingstatus = db.Column(db.Integer)
+
+def __init__(self, userid, carid, bdate, stime, etime, bookingstatus ):
+        self.userid = userid
+        self.carid = carid
+        self.bdate = bdate
+        self.stime = stime
+        self.etime = etime
+        self.bookingstatus = bookingstatus
+
+class BookingSchema(ma.Schema):    
+    class Meta:
+        # Fields to expose.
+        fields = ("bookingid", "userid", "carid", "bdate", "stime", "etime", "bookingstatus" )
+
+bookingSchema = BookingSchema()
+bookingsSchema = BookingSchema(many = True)
+
+# Get all cars
+@api.route("/api/car", methods = ["GET"])
+def getCar():
+    car = Car.query.all()
+    result = carsSchema.dump(car)
+    print(result)
+    return jsonify(result)
+
+# Get all car makes
+@api.route("/api/car/make", methods = ["GET"])
+def getMake():
+    make = CarMake.query.all()
+    result = carmakesSchema.dump(make)
+    print(result)
+    return jsonify(result)
+
+# Get all car types
+@api.route("/api/car/type", methods = ["GET"])
+def getType():
+    type = CarType.query.all()
+    result = cartypesSchema.dump(type)
+    print(result)
+    return jsonify(result)
+
+
+# Endpoint to create new car.
+@api.route("/api/car", methods = ["POST"])
+def addCar():
+    seats = request.json["seats"]
+    colour = request.json["colour"]
+    cph = request.json["cph"]
+    location = request.json["location"]
+    car_make_makeid = request.json["car_make_makeid"]
+    car_type_typeid = request.json["car_type_typeid"]
+
+    newCar = Car(seats = seats, colour = colour, cph = cph, location = location, car_make_makeid = car_make_makeid, car_type_typeid = car_type_typeid)
+
+    db.session.add(newCar)
+    db.session.commit()
+
+    return personSchema.jsonify(newCar)
+    
+    # Endpoint to create new car.
+@api.route("/api/car/booking", methods = ["POST"])
+def addCarBooking():
+    userid = request.json["userid"]
+    bdate = request.json["bdate"]
+    stime = request.json["stime"]
+    etime = request.json["etime"]
+    carid = request.json["carid"]
+    bookingstatus = request.json["bookingstatus"]
+
+    newCarBooking = Car(userid = userid, bdate = bdate, stime = stime, etime = etime, carid = carid, bookingstatus = bookingstatus)
+
+    db.session.add(newCarBookin)
+    db.session.commit()
+
+    return personSchema.jsonify(newCarBooking)
--- lib/flask_site.py
+++ lib/flask_site.py
@@ -1,8 +1,8 @@
-from flask import Flask, Blueprint, request, jsonify, render_template
+from flask import Flask, Blueprint, request, jsonify, render_template, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from passlib.hash import sha256_crypt
-import os, requests, json
+import os, time, requests, json
 
 site = Blueprint("site", __name__)
 ### User ###
@@ -34,7 +34,7 @@ def register():
             #Add account into the DB
             payload = {"email":email, "name":name, "password":password, "username":username}
             r = requests.post('http://192.168.0.199:5000/api/person', json=payload)
-            print(r.text)
+            msg = 'Congratz You have been registered......'
         else:
             msg = "Passwords do not match"
     elif request.method == 'POST':
@@ -44,11 +44,11 @@ def register():
 
 @site.route('/login', methods=['GET','POST'])
 def login():
+    msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         #Capture the form data
         username = request.form['username']
         password = request.form['password']
-        msg = ''
         if username is None or password is None:
             msg = "Username or Password empty"
         else:
@@ -56,10 +56,123 @@ def login():
             response = requests.get('http://192.168.0.199:5000/api/token', auth=(username, password))
             #make response into json format
             data = json.loads(response.text)
+            print(response.status_code)
             #take token out of json and submit it for access to user info
             token = data['token']
             response = requests.get('http://192.168.0.199:5000/api/login', auth=(token, 'unused'))
             #format the response in json
             data = json.loads(response.text)
 
-    return render_template('index.html', msg = msg)
+            #make a session for user data
+            session['loggedin'] = True
+            session['userid'] = data['userid']
+            session['username'] = data['username']
+            session['name'] = data['name']
+            session['email'] = data['email']
+            #forward the user to home page
+            msg = "Successfully logged in redirecting in 3 secounds"
+            time.sleep(3)
+            return redirect('home')
+    return render_template('index.html', msg=msg)
+
+@site.route('/home')
+def home():
+    if 'loggedin' in session:
+        #When the user is logged in show them this top secret page
+        return render_template('home.html', username=session['username'])
+    #If not bye bye
+    return redirect(url_for('login'))
+
+# Logout #
+@site.route('/logout')
+def logout():
+    #remove all session data to log the user out
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('username', None)
+    session.pop('name', None)
+    session.pop('email', None)
+    #move to login page
+    return redirect('login')
+
+# Profile Page #
+@site.route('/profile')
+def profile():
+    # check if the user is logged in
+    if 'loggedin' in session:
+        # Show the profile page with account info
+        return render_template('profile.html')
+    # User is not loggedin redirect to login page
+    return redirect('login') 
+
+
+### Car ###
+# insert car #
+@site.route('/newcar', methods=['GET', 'POST'])
+def newcar():
+    #Get car make for list
+    response = requests.get('http://192.168.0.199:5000/api/car/make')
+    #format the response in json
+    carmake = json.loads(response.text)
+
+    #Get car type for list
+    response = requests.get('http://192.168.0.199:5000/api/car/type')
+    #format the response in json
+    cartype = json.loads(response.text)
+
+    #error message
+    msg = ''
+    #checking to see if the user has pressed the submit button by looking at POST request
+    if request.method == 'POST' and 'colour' in request.form and 'seats' in request.form and 'location' in request.form and 'cph' in request.form and 'ctype' in request.form and 'cmake' in request.form: #checks post requet for all inputs
+        #Capture the form data
+        colour = request.form['colour']
+        seats = request.form['seats']
+        location = request.form['location']
+        cph = request.form['cph']
+        cmake = request.form['cmake']
+        ctype = request.form['ctype']
+        #Add account into the DB
+        if colour is None or seats is None or location is None or seats is None or cph is None or cmake is None or ctype is None:
+            msg = 'Error.... Oh Well'
+        else:
+            payload = {"colour":colour, "seats":seats, "location":location, "cph":cph, "car_make_makeid":cmake, "car_type_typeid":ctype}
+            r = requests.post('http://192.168.0.199:5000/api/car', json=payload)
+            msg = 'Congratz You have been registered......'
+    elif request.method == 'POST': #if no post request is made
+            #error message
+            msg = 'Fill the form out you ido*'
+    return render_template('newcar.html', carmake=carmake, cartype=cartype, msg=msg)
+
+
+@site.route('/newbooking', methods=['GET', 'POST'])
+def newbooking():
+    #Get cars for list
+    response = requests.get('http://192.168.0.199:5000/api/car')
+    #format the response in json
+    cars = json.loads(response.text)
+
+    #error message
+    msg = ''
+    #check to see if logged in
+    if 'loggedin' in session:
+    
+        #checking to see if the user has pressed the submit button by looking at POST request
+        if request.method == 'POST' and 'date' in request.form and 'stime' in request.form and 'etime' in request.form and 'carid' in request.form: #Get contents of post data
+            userid = session['id']
+            #Capture the form data
+            bdate = request.form['date']
+            stime = request.form['stime']
+            etime = request.form['etime']
+            carid = request.form['carid']
+            bookingstatus = 1
+            #Add account into the DB
+            if userid is None or bdate is None or stime is None or etime is None or carid is None:
+                msg = 'Error.... Oh Well'
+            else:
+                payload = {"userid":userid, "bdate":bdate, "stime":stime, "etime":etime, "carid":carid, "bookingstatus":bookingstatus}
+                r = requests.post('http://192.168.0.199:5000/api/carbooking', json=payload)
+                msg = 'Congratz You have been registered.....'
+        elif request.method == 'POST': #if no post request is made
+                #error message
+                msg = 'Fill the form out you ido*'
+    return render_template('newbooking.html', cars=cars, msg=msg)
