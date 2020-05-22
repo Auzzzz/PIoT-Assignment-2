6e0a5d8713df7ac109ea0db9390067e0a500b395
Rodrigo Miguel Rojas <s3784466@student.rmit.edu.au>
2020-05-22 23:25:52 +1000

    Fixed overwritten progress

--- socket/client.py
+++ socket/client.py
@@ -2,84 +2,116 @@
 import socket
 from getpass import getpass
 
+
 HOST = input("Enter IP address of server: ")
 
 # HOST = "127.0.0.1" # The server's hostname or IP address.
 PORT = 5004        # The port used by the server.
 ADDRESS = (HOST, PORT)
 
-def printMenu():
-    print("\nWelcome to Agent Pi Console View!")
-    print("1. Login")
-    print("2. Exit")
+class Menu:
+    def printMenu():
+        print("\nWelcome to Agent Pi Console View!")
+        print("1. Login")
+        print("2. Exit")
 
-def selectOptions():
-    print("\nPlease select from these Options")
-    print("1. Unlock Car")
-    print("2. Return Car")
-    print("3. Log Out")
+    def selectOptions():
+        print("\nPlease select from these Options")
+        print("1. Unlock Car")
+        print("2. Return Car")
+        print("3. Log Out")
 
-def login(s):
-    sent = True
-    username = "username:" + input("Please enter username: ")
-    s.sendall(username.encode())
-
-    password = getpass('Password: ')
-    passToSend = "password:" + password
-    s.sendall(passToSend.encode())
-
-    data = s.recv(4096)
-    decodedData = data.decode()
-
-    if not data:
-        sent = False
-
-    if decodedData == "Login successful":
+class Functions:
+    def login(s):
         sent = True
-    else:
-        sent = False
+        username = "username:" + input("Please enter username: ")
+        s.sendall(username.encode())
 
-    return sent
+        password = getpass('Password: ')
+        passToSend = "password:" + password
+        s.sendall(passToSend.encode())
 
+        data = s.recv(4096)
+        decodedData = data.decode()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print("Connecting to {}...".format(ADDRESS))
-    s.connect(ADDRESS)
-    print("Connected.")
+        if not data:
+            sent = False
 
-    isLoggedIn = False
-
-    #endless loop for menu until exit 
-    while True:
-        if not isLoggedIn:
-            printMenu()
-            response = input("\nResponse: ")
-            
-            if response == "1":
-                if login(s):
-                    isLoggedIn = True
-                    print("\nLogin successful!")
-                else:
-                    print("\nLogin failed")
-            elif response == "2":
-                print("\nShutting down...")
-                break
-            else:
-                print("\nError: Invalid Input")
+        if decodedData == "Login successful":
+            sent = True
         else:
-            selectOptions()
-            response = input("\nResponse: ")
+            sent = False
 
-            if response == "1":
-                print("\nUnlocking car...")
-            elif response == "2":
-                print("\nReturning car...")
-            elif response == "3":
-                print("\nLogging out...")
-                isLoggedIn = False
-            else:
-                print("\nError: Invalid Input")
+        return sent
 
+
+    def unlockCar(s):
+        unlocked = False
+        carID = "carid:" + input("Car ID: ")
+        s.sendall(carID.encode())
+        unlockCode = "unlockcode:" + input("Unlock Code: ")
+        s.sendall(unlockCode.encode())
+
+        data = s.recv(4096)
+        decodedData = data.decode()
+
+        if not data:
+            unlocked = False
     
-    print("Disconnecting from server.")
-print("Done.")
+        if decodedData == "Car Unlocked":
+            unlocked = True
+        else:
+            unlocked = False
+
+        return unlocked
+
+
+class Main:
+    def run():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Connecting to {}...".format(ADDRESS))
+            s.connect(ADDRESS)
+            print("Connected.")
+
+            isLoggedIn = False
+
+            #endless loop for menu until exit 
+            while True:
+                if not isLoggedIn:
+                    Menu.printMenu()
+                    response = input("\nResponse: ")
+            
+                    if response == "1":
+                        if Functions.login(s):
+                            isLoggedIn = True
+                            print("\nLogin successful!")
+                        else:
+                            print("\nLogin failed")
+                    elif response == "2":
+                        print("\nShutting down...")
+                        break
+                    else:
+                        print("\nError: Invalid Input")
+                else:
+                    Menu.selectOptions()
+                    response = input("\nResponse: ")
+
+                    if response == "1":
+                        if Functions.unlockCar(s):
+                            print("\nCar Unlocked")
+                        else:
+                            print("\nInvalid credentials or car already unlocked by user...")
+                    elif response == "2":
+                        print("\nReturning car...")
+                    elif response == "3":
+                        print("\nLogging out...")
+                        isLoggedIn = False
+                    else:
+                        print("\nError: Invalid Input")
+
+            print("Disconnecting from server.")
+        print("Done.")
+
+
+if __name__ == "__main__":
+    Main.run()
