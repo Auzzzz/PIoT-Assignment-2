3daf35cd877ebc8601b5dea0978bfafa35da61d2
Rodrigo Miguel Rojas <s3784466@student.rmit.edu.au>
2020-05-22 18:49:01 +1000

    Put code in socket files into classes

--- socket/server.py
+++ socket/server.py
@@ -13,72 +13,79 @@ PORT = 5004 # Port to listen on (non-privileged ports are > 1023).
 ADDRESS = (HOST, PORT)
 
 
-def login(username, password, s):
-    loginUser = username
-    loginPass = password
-    msg = ''
+class Functions:
+    def login(username, password, s):
+        loginUser = username
+        loginPass = password
+        msg = ''
 
-    if loginUser is None or loginPass is None:
-        msg = 'Username or Password is empty'
-        s.sendAll(msg.encode())
-    else:
-        response = requests.get('http://127.0.0.1:5000/api/token', auth=(loginUser, loginPass))
-        
-        #Login failed
-        if str(response) == "<Response [401]>":
-            msg = 'Unauthorized Access'
-            s.sendall(msg.encode())
-
-        #Login success
+        if loginUser is None or loginPass is None:
+            msg = 'Username or Password is empty'
+            s.sendAll(msg.encode())
         else:
-            data = json.loads(response.text)
-            token = data['token']
-            response = requests.get('http://127.0.0.1:5000/api/login', auth=(token, 'unused'))
-            data = json.loads(response.text)
-            msg = 'Login successful'
-            s.sendall(msg.encode())
-
-def addClient(conn, addr):
-    with conn:
-        print("Connected to {}".format(addr))
-        print("( -- Press CTRL+C to Quit -- )")
+            response = requests.get('http://127.0.0.1:5000/api/token', auth=(loginUser, loginPass))
         
-        sessionUser = ''
+            #Login failed
+            if str(response) == "<Response [401]>":
+                msg = 'Unauthorized Access'
+                s.sendall(msg.encode())
 
-        while True:
-            data = conn.recv(2048)
-            decodedData = data.decode()
-            if(not data):
-                break
+            #Login success
+            else:
+                data = json.loads(response.text)
+                token = data['token']
+                response = requests.get('http://127.0.0.1:5000/api/login', auth=(token, 'unused'))
+                data = json.loads(response.text)
+                msg = 'Login successful'
+                s.sendall(msg.encode())
 
-            instruct, info = decodedData.split(":", 1)
 
-            if instruct == "username":
-                sessionUser = info
-            elif instruct == "password":
-                login(sessionUser, info, conn)
+class Main:
+    def addClient(conn, addr):
+        with conn:
+            print("Connected to {}".format(addr))
+            print("( -- Press CTRL+C to Quit -- )")
+        
+            sessionUser = ''
 
-        print("Disconnecting from client " + str(addr) + "...")
-        conn.close()
+            while True:
+                data = conn.recv(2048)
+                decodedData = data.decode()
+                if(not data):
+                    break
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    try:
-        s.bind(ADDRESS)
-    except socket.error as e:
-        print(str(e))
+                instruct, info = decodedData.split(":", 1)
+
+                if instruct == "username":
+                    sessionUser = info
+                elif instruct == "password":
+                    login(sessionUser, info, conn)
+
+            print("Disconnecting from client " + str(addr) + "...")
+            conn.close()
+
+    def run():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(ADDRESS)
+            except socket.error as e:
+                print(str(e))
      
-    s.listen()
+            s.listen()
 
-    while True:
-        try:
-            print("Listening on {}...".format(ADDRESS))
-            conn, addr = s.accept()
-            start_new_thread(addClient, (conn, addr,))
-        except KeyboardInterrupt:
-            break
+            while True:
+                try:
+                    print("Listening on {}...".format(ADDRESS))
+                    conn, addr = s.accept()
+                    start_new_thread(addClient, (conn, addr,))
+                except KeyboardInterrupt:
+                    break
 
-    s.shutdown(socket.SHUT_RDWR)
-    s.close()
-    print("\nClosing listening socket...")
-print("Done!")
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            print("\nClosing listening socket...")
+        print("Done!")
+
+if __name__ == "__main__":
+    Main.run()
             
